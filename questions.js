e1abdbe3bfb7dc7df9842e9acbe9d87bc6b11906
Mark <mark.dessain@gmail.com>
2013-10-26 18:10:24 +0100

    moving list out into new file

---
+++ questions.js
@@ -0,0 +1,44 @@
+data = {
+    "words": {
+        "Hello": ["CY"],
+        "Goodbye": ["CY"],
+        "Yes": ["CY"],
+        "No": ["CY"],
+        "Thanks": ["CY"],
+        "Sorry": ["CY"],
+        "Now": ["CY"],
+        "Later": ["CY"],
+        "Water": ["CY"],
+        "Toilet": ["CY"],
+        "Rain": ["CY"],
+        "Snow": ["CY"],
+        "Hot": ["CY"],
+        "Cold": ["CY"],
+        "Dog": ["CY"],
+        "Cat": ["CY"]
+    },
+    "short_questions": {
+        "Where are you going?": ["CY"],
+        "What do you want?": ["CY"],
+        "Do you need this?": ["CY"],
+        "When are you leaving?": ["CY"]
+    },
+    "short_statements": {
+		"It is not here.": ["CY"], 
+        "The train has left.": ["CY"],
+        "Please come with me.": ["CY"],
+        "I can't help you": ["CY"]
+    },
+    "long_questions": {
+        "You can't go there now.": ["CY"],
+        "You must take a train there.": ["CY"],
+        "It is very far from here.": ["CY"],
+        "I don't know where it is.": ["CY"]
+    },
+    "long_statements": {
+        "What would you like to drink?": ["CY"],
+        "Where do you want to go tomorrow?": ["CY"],
+        "Would you like anything else now?": ["CY"],
+        "Do you know how to go there?": ["CY"]
+    }
+}
