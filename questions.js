1cbfc877da06b794a6b5c6c6f2b1bc3c763bbb6b
Mark <mark.dessain@gmail.com>
2013-10-26 18:32:31 +0100

    Adding languages

--- questions.js
+++ questions.js
@@ -1,44 +1,44 @@
 data = {
     "words": {
-        "Hello": ["CY"],
-        "Goodbye": ["CY"],
-        "Yes": ["CY"],
-        "No": ["CY"],
-        "Thanks": ["CY"],
-        "Sorry": ["CY"],
-        "Now": ["CY"],
-        "Later": ["CY"],
-        "Water": ["CY"],
-        "Toilet": ["CY"],
-        "Rain": ["CY"],
-        "Snow": ["CY"],
-        "Hot": ["CY"],
-        "Cold": ["CY"],
-        "Dog": ["CY"],
-        "Cat": ["CY"]
+        "Hello": ["CY", "ES", "ZH", "FR"],
+        "Goodbye": ["CY", "ES", "ZH", "FR"],
+        "Yes": ["CY", "ES", "ZH", "FR"],
+        "No": ["CY", "ES", "ZH", "FR"],
+        "Thanks": ["CY", "ES", "ZH", "FR"],
+        "Sorry": ["CY", "ES", "ZH", "FR"],
+        "Now": ["CY", "ES", "ZH", "FR"],
+        "Later": ["CY", "ES", "ZH", "FR"],
+        "Water": ["CY", "ES", "ZH", "FR"], 
+        "Toilet": ["CY", "ES", "ZH", "FR"],
+        "Rain": ["CY", "ES", "ZH", "FR"],
+        "Snow": ["CY", "ES", "ZH", "FR"],
+        "Hot": ["CY", "ES", "ZH", "FR"],
+        "Cold": ["CY", "ES", "ZH", "FR"],
+        "Dog": ["CY", "ES", "ZH", "FR"],
+        "Cat": ["CY", "ES", "ZH", "FR"]
     },
     "short_questions": {
-        "Where are you going?": ["CY"],
-        "What do you want?": ["CY"],
-        "Do you need this?": ["CY"],
-        "When are you leaving?": ["CY"]
+        "Where are you going?": ["CY", "ES", "ZH", "FR"],
+        "What do you want?": ["CY", "ES", "ZH", "FR"],
+        "Do you need this?": ["CY", "ES", "ZH", "FR"],
+        "When are you leaving?": ["CY", "ES", "ZH", "FR"]
     },
     "short_statements": {
-		"It is not here.": ["CY"], 
-        "The train has left.": ["CY"],
-        "Please come with me.": ["CY"],
-        "I can't help you": ["CY"]
+		"It is not here.": ["CY", "ES", "ZH", "FR"], 
+        "The train has left.": ["CY", "ES", "ZH", "FR"],
+        "Please come with me.": ["CY", "ES", "ZH", "FR"],
+        "I can't help you": ["CY", "ES", "ZH", "FR"]
     },
     "long_questions": {
-        "You can't go there now.": ["CY"],
-        "You must take a train there.": ["CY"],
-        "It is very far from here.": ["CY"],
-        "I don't know where it is.": ["CY"]
+        "You can't go there now.": ["CY", "ES", "ZH", "FR"],
+        "You must take a train there.": ["CY", "ES", "ZH", "FR"],
+        "It is very far from here.": ["CY", "ES", "ZH", "FR"],
+        "I don't know where it is.": ["CY", "ES", "ZH", "FR"]
     },
     "long_statements": {
-        "What would you like to drink?": ["CY"],
-        "Where do you want to go tomorrow?": ["CY"],
-        "Would you like anything else now?": ["CY"],
-        "Do you know how to go there?": ["CY"]
+        "What would you like to drink?": ["CY", "ES", "ZH", "FR"],
+        "Where do you want to go tomorrow?": ["CY", "ES", "ZH", "FR"],
+        "Would you like anything else now?": ["CY", "ES", "ZH", "FR"],
+        "Do you know how to go there?": ["CY", "ES", "ZH", "FR"]
     }
 }
