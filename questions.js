a6d76258b95601c29d8a291f6d1503d2e344f6f3
Rob Finean <rfinean@iee.org>
2013-10-27 00:10:08 +0100

    renamed languages to English names (eg CY -> Welsh)

--- questions.js
+++ questions.js
@@ -2,54 +2,54 @@
 
 var data = {
     "words": {
-        "Hello": ["CY", "ES", "ZH", "FR"],
-        "Goodbye": ["CY", "ES", "ZH", "FR"],
-        "Here": ["CY", "ES", "ZH", "FR"],
-        "There": ["CY", "ES", "ZH", "FR"],
-        "Thanks": ["CY", "ES", "ZH", "FR"],
-        "Sorry": ["CY", "ES", "ZH", "FR"],
-        "Now": ["CY", "ES", "ZH", "FR"],
-        "Later": ["CY", "ES", "ZH", "FR"],
-        "Water": ["CY", "ES", "ZH", "FR"],
-        "Toilet": ["CY", "ES", "ZH", "FR"],
-        "Rain": ["CY", "ES", "ZH", "FR"],
-        "Snow": ["CY", "ES", "ZH", "FR"],
-        "Hot": ["CY", "ES", "ZH", "FR"],
-        "Cold": ["CY", "ES", "ZH", "FR"],
-        "Dog": ["CY", "ES", "ZH", "FR"],
-        "Cat": ["CY", "ES", "ZH", "FR"]
+        "Hello": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Goodbye": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Here": ["Welsh", "Spanish", "Mandarin", "French"],
+        "There": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Thanks": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Sorry": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Now": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Later": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Water": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Toilet": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Rain": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Snow": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Hot": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Cold": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Dog": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Cat": ["Welsh", "Spanish", "Mandarin", "French"]
     },
     "short_questions": {
-        "Where are you going?": ["CY", "ES", "ZH", "FR"],
-        "What do you want?": ["CY", "ES", "ZH", "FR"],
-        "Do you need this?": ["CY", "ES", "ZH", "FR"],
-        "Do you understand me?": ["CY", "ES", "ZH", "FR"],
-        "Where are you from?": ["CY", "ES", "ZH", "FR"],
-        "When are you leaving?": ["CY", "ES", "ZH", "FR"]
+        "Where are you going?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "What do you want?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Do you need this?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Do you understand me?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Where are you from?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "When are you leaving?": ["Welsh", "Spanish", "Mandarin", "French"]
     },
     "short_statements": {
-		"It is not here.": ["CY", "ES", "ZH", "FR"],
-        "The train has left.": ["CY", "ES", "ZH", "FR"],
-        "Please come with me.": ["CY", "ES", "ZH", "FR"],
-        "Don't worry about it.": ["CY", "ES", "ZH", "FR"],
-        "You have to wait.": ["CY", "ES", "ZH", "FR"],
-        "Let's go now.": ["CY", "ES", "ZH", "FR"],
-        "I can't help you": ["CY", "ES", "ZH", "FR"]
+		"It is not here.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "The train has left.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Please come with me.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Don't worry about it.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "You have to wait.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Let's go now.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "I can't help you": ["Welsh", "Spanish", "Mandarin", "French"]
     },
     "long_statements": {
-        "You can't go there now.": ["CY", "ES", "ZH", "FR"],
-        "You must take the train there.": ["CY", "ES", "ZH", "FR"],
-        "It is very far from here.": ["CY", "ES", "ZH", "FR"],
-        "You have to buy a ticket here.": ["CY", "ES", "ZH", "FR"],
-        "You can go to the beach tonight.": ["CY", "ES", "ZH", "FR"],
-        "I don't know where it is.": ["CY", "ES", "ZH", "FR"]
+        "You can't go there now.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "You must take the train there.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "It is very far from here.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "You have to buy a ticket here.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "You can go to the beach tonight.": ["Welsh", "Spanish", "Mandarin", "French"],
+        "I don't know where it is.": ["Welsh", "Spanish", "Mandarin", "French"]
     },
     "long_questions": {
-        "What would you like to drink?": ["CY", "ES", "ZH", "FR"],
-        "Where do you want to go tomorrow?": ["CY", "ES", "ZH", "FR"],
-        "Would you like anything else now?": ["CY", "ES", "ZH", "FR"],
-        "Can you come back tomorrow morning?": ["CY", "ES", "ZH", "FR"],
-        "Do you want to taste the cake?": ["CY", "ES", "ZH", "FR"],
-        "Do you know how to go there?": ["CY", "ES", "ZH", "FR"]
+        "What would you like to drink?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Where do you want to go tomorrow?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Would you like anything else now?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Can you come back tomorrow morning?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Do you want to taste the cake?": ["Welsh", "Spanish", "Mandarin", "French"],
+        "Do you know how to go there?": ["Welsh", "Spanish", "Mandarin", "French"]
     }
 };
